45d7f60a7ab56929c3f7da77824d6cb0ee65fed1
kotarowakuwaku <is0668fs@ed.ritsumei.ac.jp>
2025-04-10 21:35:25 +0900

    TextInputにidをわたす

--- src/app/components/Quiz.tsx
+++ src/app/components/Quiz.tsx
@@ -9,7 +9,7 @@ type QuizProps = {
     title: string;
 } & YoutubePlayerProps & TextInputProps;
 
-const Quiz = ({ title, videoId, introDuration, value, onChange }: QuizProps) => {
+const Quiz = ({ title, videoId, introDuration, id, value, onChange }: QuizProps) => {
     return (
         <>
         <div style={{position:"relative", width:"100%", border:"1px solid black", padding:"1em 0.5em 0.5em 0.5em", borderRadius:"10px", margin:"1em"}}>
@@ -17,7 +17,7 @@ const Quiz = ({ title, videoId, introDuration, value, onChange }: QuizProps) => 
             <div style={{ display: "flex", justifyContent: "flex-start", width: "100%"}}>
                 <YoutubePlayer key={videoId} videoId={videoId} introDuration={introDuration} />
                 <div style={{margin:"0 10px"}}></div>
-                <TextInput value={value} onChange={onChange} />
+                <TextInput id={id} value={value} onChange={onChange} />
             </div>
         </div>
         </>
